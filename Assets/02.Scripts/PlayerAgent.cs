a886ca3f0e31161a734bd65cf5b09b761090fa6e
JaeHyun Lee <myleje@gmail.com>
2022-02-16 15:02:25 +0900

    .

--- Assets/02.Scripts/PlayerAgent.cs
+++ Assets/02.Scripts/PlayerAgent.cs
@@ -95,7 +95,7 @@ public class PlayerAgent : Agent
 
         // Branch 1
         if (Input.GetKey(KeyCode.A)) actions[1] = 1;
-        if (Input.GetKey(KeyCode.D)) actions[1] = 1;
+        if (Input.GetKey(KeyCode.D)) actions[1] = 2;
     }
 
 }
